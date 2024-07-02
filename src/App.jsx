b065b2f9cc71b724133ee775032031901ded818e
CarlosVasque2 <carlos.vasquez.jones@gmail.com>
2024-07-01 20:22:34 -0400

    Creación de carpetas y archivos para el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import NavBar from "./components/NavBar/NavBar"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+
+
+function App() {
+  const [cartCount, setCartCount] = useState(5)
+
+  return (
+    <>
+      <NavBar cartCount={cartCount}/>
+      <ItemListContainer greetings="Bienvenido a nuestra tienda Plata Facil"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+
+const categorias = ['Pantalones', 'Poleras', 'Polerones', 'Shorts', 'Gorras']
+
+function ItemListContainer({greetings}) {
+    //console.log(categorias)
+  return (
+    <>
+    <h2>{greetings}</h2>
+    </>
+  )
+}
+
+export default ItemListContainer
